757a8a2506e930cd8268134b8c8730526816dca3
khushi678 <khushi.mehta678@gmail.com>
2020-10-02 09:33:02 +0530

    changed search bar

--- src/components/homepage/homepage.component.js
+++ src/components/homepage/homepage.component.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
 import "./homepage.component.css"
+import Carousel from 'react-bootstrap/Carousel'
 
 export default class Homepage extends Component {
     render() {
+
         return (
             <div className="homepage-main" style={{ color: "whitesmoke" }}>
-                Here there be the Dashboard
+                
+                <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=First slide&bg=373940"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Second slide&bg=282c34"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Third slide&bg=20232a"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
             </div>
         )
     }
