a36114baf27d1b5b0c07cc7f9ea2b3c369234c76
khushi678 <khushi.mehta678@gmail.com>
2020-10-03 15:40:50 +0530

    carousel

--- src/components/homepage/homepage.component.js
+++ src/components/homepage/homepage.component.js
@@ -1,23 +1,67 @@
 import React, { Component } from 'react';
 import "./homepage.component.css"
 import Carousel from 'react-bootstrap/Carousel'
+import Image from 'react-bootstrap/Image'
+import Card from 'react-bootstrap/Card'
+import CardDeck from 'react-bootstrap/CardDeck'
 
 export default class Homepage extends Component {
     render() {
 
         return (
             <div className="homepage-main" style={{ color: "whitesmoke" }}>
-                
+
+<CardDeck>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This content is a little bit longer.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This card has supporting text below as a natural lead-in to additional
+        content.{' '}
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This card has even longer content than the first to
+        show that equal height action.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+</CardDeck>  
+              
                 <Carousel>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=First slide&bg=373940"
-          alt="First slide"
-        />
+        
+          <Image src="desktop1.js/100px250" fluid />
+         
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h3>Lenovo IdeaCentre AIO 330 19.5-inch All-in-One Desktop </h3>
+          <p>₹ 25,000</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
