61d4a68c1ae86104c1a6541211ee42cd0c5ddb10
khushi678 <khushi.mehta678@gmail.com>
2020-10-13 13:20:25 +0530

    modifications

--- src/components/homepage/homepage.component.js
+++ src/components/homepage/homepage.component.js
@@ -1,15 +1,98 @@
 import React, { Component } from 'react';
 import "./homepage.component.css"
+import Carousel from 'react-bootstrap/Carousel'
+import Image from 'react-bootstrap/Image'
+import Card from 'react-bootstrap/Card'
+import CardDeck from 'react-bootstrap/CardDeck'
 
 export default class Homepage extends Component {
-  render() {
+    render() {
 
-    return (
-      <div className="homepage-main">
-        Esse ratione dolorem aut provident ut. Possimus eum et quidem rerum. Necessitatibus quia iste nesciunt et laudantium. Et culpa et voluptatibus officia. Qui enim quasi aut a dolorem exercitationem. Quisquam quidem culpa aspernatur ullam tempore amet vitae ex error.
-        Cupiditate ut quam autem et animi. Aliquam ut harum expedita qui. Nulla architecto tempora nihil quia qui officia incidunt quo praesentium.
-        Dolorum numquam repudiandae itaque laborum quo possimus rerum rerum. Maiores quo non dolorum laudantium recusandae voluptas quia quia necessitatibus. Perspiciatis ipsa ut sint et sit aliquid ut eum.
-      </div>
-    )
-  }
+        return (
+            <div className="homepage-main" style={{ color: "whitesmoke" }}>
+
+              
+          <Carousel>
+                <Carousel.Item>
+                  
+                    <Image src="desktop1.js/100px250" fluid />
+                  
+                  <Carousel.Caption>
+                    <h3>Lenovo IdeaCentre AIO 330 19.5-inch All-in-One Desktop </h3>
+                    <p>₹ 25,000</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-100"
+                    src="holder.js/800x400?text=Second slide&bg=282c34"
+                    alt="Second slide"
+                  />
+
+                  <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-100"
+                    src="holder.js/800x400?text=Third slide&bg=20232a"
+                    alt="Third slide"
+                  />
+
+                  <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>
+                      Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    </p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              </Carousel>
+              
+          <CardDeck>
+            <Card>
+              <Card.Img variant="top" src="holder.js/100px160" />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural lead-in to
+                  additional content. This content is a little bit longer.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </Card.Footer>
+            </Card>
+            <Card>
+              <Card.Img variant="top" src="holder.js/100px160" />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This card has supporting text below as a natural lead-in to additional
+                  content.{' '}
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </Card.Footer>
+            </Card>
+            <Card>
+              <Card.Img variant="top" src="holder.js/100px160" />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural lead-in to
+                  additional content. This card has even longer content than the first to
+                  show that equal height action.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </Card.Footer>
+            </Card>
+          </CardDeck>
+</div>
+        )
+    }
 }
