76eadde75892a71fd9b2cab976c68bbb9047679a
Argha Chakrabarty <ac380012@gmail.com>
2020-10-14 22:41:52 +0530

    category0.9

--- src/components/category/category.component.js
+++ src/components/category/category.component.js
@@ -6,32 +6,45 @@ import axios from "axios";
 
 const data = [
     {
-        "id": "gameboy",
+        "pictures": [
+            "https://illlustrations.co/static/cb0069bee07d4675ef939a4c61774cac/116-gameboy.png",
+            "https://64.media.tumblr.com/e5f1772c9315615fd54d0727f762c3d5/tumblr_o3paohrAl31v8mn5wo1_1280.jpg"
+        ],
         "category": "Vintage Gaming",
-        "productName": "Gameboy",
+        "_id": "5f8226bd85da3239dc1f5c25",
+        "name": "Gameboy",
         "description": "Nintendo’s GameBoy made its Japanese debut on April 21, 1989. With a murky screen and chunky physical design, Game Boy wasn’t the most impressive of game systems — but what it lacked in power, it made up for in affordability ... and, over time, an incredible library. Ask any Game Boy owner for a list of their favorite games and you’ll get a huge variety of answers thanks to the fact that the system saw north of 1000 games over its lifetime, many of which were good and some of which were truly great and Pokemon is love.",
         "price": "₹2000",
-        "imgLinks": ["https://illlustrations.co/static/cb0069bee07d4675ef939a4c61774cac/116-gameboy.png", "https://64.media.tumblr.com/e5f1772c9315615fd54d0727f762c3d5/tumblr_o3paohrAl31v8mn5wo1_1280.jpg"]
-    },
-    {
-        "id": "nintendo64",
-        "category": "Vintage Gaming",
-        "productName": "Nintendo 64",
-        "description": "The Nintendo 64 was one of the first gaming consoles to have four controller ports.The most graphically demanding Nintendo 64 games that arrived on larger 32 or 64 MB cartridges are the most advanced and detailed of the 32-bit/64-bit generation. In order to maximize use of the Nintendo 64 hardware developers had to create their own custom microcode. Nintendo 64 games running on custom microcode benefited from much higher polygon counts in tandem with more advanced lighting, animation, physics and AI routines than its 32-bit competition. ",
-        "price": "₹7000",
-        "imgLinks": ["https://core-electronics.com.au/media/catalog/product/8/b/8bitdo-n64-primo.jpg", "https://illlustrations.co/static/7fdea21037f680a946e430532feed521/112-installing.svg"]
     }
 ]
 
 export default function Category() {
-    const [productList, setProductList] = useState("");
+    const [productList, setProductList] = useState([{
+        "pictures": [
+            "https://illlustrations.co/static/cb0069bee07d4675ef939a4c61774cac/116-gameboy.png",
+            "https://64.media.tumblr.com/e5f1772c9315615fd54d0727f762c3d5/tumblr_o3paohrAl31v8mn5wo1_1280.jpg"
+        ],
+        "category": "",
+        "_id": "",
+        "name": "",
+        "description": "",
+        "price": "",
+    }]);
+
+    const fetchData = () => {
+        axios.get(`api/products/`)
+            .then((response) => {
+                // const setItem = 
+                setProductList(response.data.products)
+            })
+            .catch(err => console.log(`${err} from catergory.component frontend`))
+    }
 
     useEffect(() => {
-        axios.get(`/api/products/`)
-            .then(response => {
-                setProductList(response.data.products)
-            }).catch(err => console.log(`${err} from catergory.component frontend`))
-    }, [productList])
+        setTimeout(() => {
+            fetchData();
+        }, 1000);
+    }, [])
 
     return(
         <div className = "category-main" >
@@ -40,12 +53,10 @@ export default function Category() {
             <br />
             <div className="cat-contents">
                 {
-                    // productList.map(() => {
-                    //     <ItemCategory />
-                    // })
+                    productList.map((index) => (
+                        index.category==="Vintage Gaming"?<ItemCategory data={index} />:null
+                    ))
                 }
-                <ItemCategory data={productList[0]} />
-                {/* <ItemCategory /> */}
             </div>
         </div>
     )
--- src/components/category/item.category.component.js
+++ src/components/category/item.category.component.js
@@ -3,14 +3,6 @@ import { Link } from "react-router-dom";
 
 const data = [
     {
-        "id": "gameboy",
-        "category": "Vintage Gaming",
-        "productName": "Gameboy",
-        "description": "Nintendo’s GameBoy made its Japanese debut on April 21, 1989. With a murky screen and chunky physical design, Game Boy wasn’t the most impressive of game systems — but what it lacked in power, it made up for in affordability ... and, over time, an incredible library. Ask any Game Boy owner for a list of their favorite games and you’ll get a huge variety of answers thanks to the fact that the system saw north of 1000 games over its lifetime, many of which were good and some of which were truly great and Pokemon is love.",
-        "price": "₹2000",
-        "imgLinks": ["https://illlustrations.co/static/cb0069bee07d4675ef939a4c61774cac/116-gameboy.png", "https://64.media.tumblr.com/e5f1772c9315615fd54d0727f762c3d5/tumblr_o3paohrAl31v8mn5wo1_1280.jpg"]
-    },
-    {
         "pictures": [
             "https://illlustrations.co/static/cb0069bee07d4675ef939a4c61774cac/116-gameboy.png",
             "https://64.media.tumblr.com/e5f1772c9315615fd54d0727f762c3d5/tumblr_o3paohrAl31v8mn5wo1_1280.jpg"
@@ -25,9 +17,11 @@ const data = [
 
 export default function ItemCategory(props) {
 
+    console.log(props.data);
+
     return(
         <div className = "cat-item" >
-            <Link to={`/product/${props.data._id}`} 
+            <Link to={`/product/${props.data._id}`}
             className="cat-item-title cat-title"
             style={{ "color": "#6b6b6b" }}>
                 {props.data.name}
