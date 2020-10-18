6fe377c947451f389c58ed0bf9a16676e966a68a
Aarav Varshney <aaravvarshney22@gmail.com>
2020-10-18 13:28:01 +0530

    props moved to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // TODO Fix the footer thing. 
 
-import React, { Component } from 'react'; 
+import React, { useState, useEffect } from 'react'; 
 import { Route, withRouter } from 'react-router-dom';
 import axios from 'axios';
 
@@ -14,7 +14,7 @@ import Product from "./components/product/product.component"
 import Homepage from "./components/homepage/homepage.component"
 
 // Aarav edits: 
-import LandingPage from "./components/landing_page"
+import { GoogleLogin, GoogleLogout, useGoogleLogin } from 'react-google-login';
 
 import Category from "./components/category/category.component"
 import Cart from "./components/cart/cart.component";
@@ -27,23 +27,47 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'; 
 
 
-export class App extends Component  {
-  // componentDidMount() {
-  //   axios.get('/api/user-data')
-  //   .then(res => {
-  //     const {dispatch} = this.props; // Destructing dispatch to initiate actions
-  //     if (res.data.user) {
-  //       dispatch(login(res.data.user)); 
-  //     } else {
-  //       dispatch(logout());
-  //     }
-  //   }) 
-  // }
+function App()  {
+  const [isSignedIn, setSignedIn] = useState(false);
+  const [userId, setUserID] = useState('');
+  const [user, setUser] = useState(null);
 
-  render() {
+  const logoutSuccess = () => {
+    console.log("From Logout: ", userId)
+    axios({
+      method: "POST",
+      url: "http://localhost:5000/api/google_logout",
+      data: { userId: userId }
+    }).then(res => {
+      setSignedIn(false);
+      setUser(null);
+      // dispatch(logout());
+    })
+  }
+
+  const sucessfulResponseGoogle = (response) => {
+    axios({
+      method: "POST",
+      url: 'http://localhost:5000/api/google_login',
+      data: { tokenId: response.tokenId }
+    }).then(res => {
+      console.log(res);
+      setSignedIn(true);
+      setUserID(res.data.user._id);
+      setUser(res.data.user);
+      console.log("Var inside: ", isSignedIn, res.data.user._id);
+    })
+      .catch(err => console.log("Error from succesfulResponseGoogle", err));
+  }
+
+  const failedResponseGoogle = (response) => {
+    console.log("Here", response);
+    setSignedIn(true);
+  }
+
     return (
         <div className="main">
-          <NavBar />
+          <NavBar user = {user} isSignedIn = {isSignedIn} logoutSuccess = {logoutSuccess} sucessfulResponseGoogle = {sucessfulResponseGoogle} failedResponseGoogle = {failedResponseGoogle}/>
           <br />
           <Route path="/" exact component={Homepage} />
           <Route path="/product/:productId" component={Product}/>
@@ -56,6 +80,5 @@ export class App extends Component  {
         </div>
     );
   }
-}
 
-export default withRouter(connect()(App)); //Connecting app with reducer
+export default App; // withRouter(connect()(App)); //Connecting app with reducer
--- src/components/navbar/navbar.component.js
+++ src/components/navbar/navbar.component.js
@@ -15,7 +15,6 @@ import { Navbar } from "react-bootstrap"
 import "./navbar.component.css"
 
 export default function NavBar(props) {
-    // const countries = ['India', 'Nepal', 'Bangladesh'];
     const countries = [
       {name: 'India', value: 'sv'},
       {name: 'Nepal', value: 'en'},
@@ -31,10 +30,10 @@ export default function NavBar(props) {
       )
     }
 
-  const dispatch = useDispatch();
+
   const [sidebarToggle, setToggle] = useState(true);
-  const [isSignedIn, setSignedIn] = useState(false);
-  const [userId, setUserID] = useState('');
+  // const [isSignedIn, setSignedIn] = useState(false);
+  // const [userId, setUserID] = useState('');
   const [user, setUser] = useState(null);
   const [search, setSearch] = useState("");
   const [productList, setProductList] = useState([]);
@@ -79,58 +78,21 @@ export default function NavBar(props) {
     setToggle(!sidebarToggle);
   }
 
-  // const { loginWithRedirect, logout, user, isAuthenticated, isLoading } = useAuth0();
-  // console.log("Authentication Status: ", isAuthenticated);
-
-  const logoutSuccess = () => {
-    console.log("From Logout: ", userId)
-    axios({
-      method: "POST",
-      url: "http://localhost:5000/api/google_logout",
-      data: { userId: userId }
-    }).then(res => {
-      setSignedIn(false);
-      setUser(null);
-      // dispatch(logout());
-    })
-  }
-
-  const sucessfulResponseGoogle = (response) => {
-    axios({
-      method: "POST",
-      url: 'http://localhost:5000/api/google_login',
-      data: { tokenId: response.tokenId }
-    }).then(res => {
-      console.log(res);
-      setSignedIn(true);
-      setUserID(res.data.user._id);
-      setUser(res.data.user);
-      console.log("Var inside: ", isSignedIn, res.data.user._id);
-      // dispatch(login(res.data.user));
-    })
-      .catch(err => console.log("Error from succesfulResponseGoogle", err));
-  }
-
-  const failedResponseGoogle = (response) => {
-    console.log("Here", response);
-    setSignedIn(true);
-  }
-
   var auth_button;
-  console.log("Var Outside: ", isSignedIn)
-  if (isSignedIn) {
+  console.log("Var Outside: ", props.isSignedIn)
+  if (props.isSignedIn) {
     auth_button = <GoogleLogout
       clientId="741634897739-ac07i81bga1jtqdg7lqfk98tt71m76h5.apps.googleusercontent.com"
       buttonText="Logout"
-      onLogoutSuccess={logoutSuccess}
+      onLogoutSuccess={props.logoutSuccess}
     >
     </GoogleLogout>
   } else {
     auth_button = <GoogleLogin
       clientId="741634897739-ac07i81bga1jtqdg7lqfk98tt71m76h5.apps.googleusercontent.com"
       buttonText="Login with Google"
-      onSuccess={sucessfulResponseGoogle}
-      onFailure={failedResponseGoogle}
+      onSuccess={props.sucessfulResponseGoogle}
+      onFailure={props.failedResponseGoogle}
       cookiePolicy={'single_host_origin'}
     />
   }
@@ -138,10 +100,7 @@ export default function NavBar(props) {
 
   return (
     <div className="nav-main">
-      <div>
-        {/* {isSignedIn?console.log("user from navbar: ", user): console.log('')} */}
-        {user? <ProductPage user={user} /> : null}
-      </div>
+      
       <Navbar fixed="top" style={{ "justifyContent": "space-between", "padding": "0.5rem 0.5rem" }}>
 
         <div className="left-side">
@@ -155,12 +114,6 @@ export default function NavBar(props) {
           </span>
 
           <form className="form-main">
-            {/* <SelectSearch
-              options={countries}
-              search
-              placeholder="Select your country"
-            /> */}
-            {/* {search} */}
             <input placeholder="Search" type="text" className="search-form" onChange={ e => setSearch(e.target.value)} />
             <button className="search-btn"><i className="fas fa-search"></i></button>
             {
@@ -179,13 +132,6 @@ export default function NavBar(props) {
         <div className="right-side">
           <button className="cart-btn"><i className="fas fa-shopping-cart fa-lg"></i></button>
           {auth_button}
-          {/* <GoogleLogin
-            clientId="741634897739-ac07i81bga1jtqdg7lqfk98tt71m76h5.apps.googleusercontent.com"
-            buttonText="Login"
-            onSuccess={sucessfulResponseGoogle}
-            onFailure={failedResponseGoogle}
-            cookiePolicy={'single_host_origin'}
-        /> */}
         </div>
       </Navbar>
     </div>
