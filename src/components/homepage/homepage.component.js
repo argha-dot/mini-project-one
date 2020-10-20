e7e6ea53b964b347b4d7637650aeabca37b8c4be
khushi678 <khushi.mehta678@gmail.com>
2020-10-21 02:48:35 +0530

    database

--- src/components/homepage/homepage.component.js
+++ src/components/homepage/homepage.component.js
@@ -6,15 +6,15 @@ import axios from "axios"
 import "./homepage.component.css";
 
 const category = [["Motherboard", "https://images.unsplash.com/photo-1510746001195-0db09655b6db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80", "motherboard"],
-                ["Memory", "https://illlustrations.co/static/5d79f1332ba62184353e36f18414d446/day43-ram.svg", "ram"],
-                ["CPU", "https://images.unsplash.com/photo-1510746001195-0db09655b6db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80", "cpu"],
-                ["Storage", "https://illlustrations.co/static/c27ccee20e91c4ebc7234269ebe53498/day18-floppy.svg", "storage"],
-                ["Cabinets", "https://illlustrations.co/static/40a0a907b32106ca33b171266df6b66b/day41-desktop.svg", "cabinets"],
-                ["Peripherals", "https://illlustrations.co/static/9d3a8eb72777973c7712fa0bd8a87343/day70-designer-fav-tool-wacom.svg", "peri"],
-                ["Graphics Card", "https://illlustrations.co/static/e196df89d9960fec2923f5452aa45796/day45-gpu-ati.svg", "graphics"],
+                ["Memory", "https://images.unsplash.com/photo-1562976540-1502c2145186?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1778&q=80", "ram"],
+                ["CPU", "https://images.unsplash.com/photo-1555617778-02518510b9fa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80", "cpu"],
+                ["Storage", "https://images.unsplash.com/photo-1531492746076-161ca9bcad58?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80", "storage"],
+                ["Cabinets", "https://images.unsplash.com/photo-1592664474496-8f2c35acd655?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", "cabinets"],
+                ["Peripherals", "https://images.unsplash.com/photo-1595074475126-11362260b8aa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=752&q=80", "peri"],
+                ["Graphics Card", "https://images.unsplash.com/photo-1591489378430-ef2f4c626b35?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80", "graphics"],
                 ["Power Units/Cooling", "https://images.unsplash.com/photo-1562976540-1502c2145186?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1778&q=80", "psu"],
-                ["Monitors", "https://illlustrations.co/static/400a443144902a220c67635c9e940fbd/day42-imac.svg", "monitor"],
-                ["For Gamers", "https://illlustrations.co/static/2bdf32f5efb298073fbe6e29689ac86a/day2-gaming-console.svg", "games"]
+                ["Monitors", "https://images.unsplash.com/photo-1547658718-1cdaa0852790?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80", "monitor"],
+                ["For Gamers", "https://images.unsplash.com/photo-1599005280793-04627b9bd10a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80", "games"]
               ]
 
 const chooseRandom = (arr, num = 1) => {
